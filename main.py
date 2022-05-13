af1dd90a2fbfebe78c0084f97c19f1e24635c54a
rockstarcubalife <104049632+rockstarcubalife@users.noreply.github.com>
2022-05-13 00:08:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -237,10 +237,10 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+        tl_admin_user = os.environ.get('tl_admin_user','*')
 
         #Descomentar debajo solo si se ba a poner el usuario admin de telegram manual
-        #tl_admin_user = os.environ.get('administrador')
+        #tl_admin_user = '*'
         jdb = JsonDatabase('database')
         jdb.check_create()
         jdb.load()
